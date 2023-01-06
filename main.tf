14f023d90ba3ba97d88e1cb28c4aaa5f35425c7b
Hussain Hassani <hussainhassani@hotmail.com>
2023-01-06 09:06:03 +0000

    fixed issues with script

--- main.tf
+++ main.tf
@@ -1,38 +1,45 @@
-# provider "docker" {}
-# provider "random" {}
+provider "docker" {}
+provider "random" {}
 
-# resource "docker_image" "nginx" {
-#   name         = "nginx:latest"
-#   keep_locally = false
-# }
+resource "docker_image" "nginx" {
+  name         = "nginx:latest"
+  keep_locally = false
+}
 
-# resource "docker_container" "nginx" {
-#   image = docker_image.nginx.latest
-#   name  = "hello-terraform"
-#   ports {
-#     internal = 80
-#     external = 8000
-#   }
-# }
+resource "docker_container" "nginx" {
+  image = docker_image.nginx.latest
+  name  = "hello-terraform"
+  ports {
+    internal = 80
+    external = 8000
+  }
+}
 
-# resource "random_pet" "dog" {
-#   length = 2
-# }
+resource "random_pet" "dog" {
+  length = 2
+}
 
-# module "nginx-pet" {
-#   source = "./nginx"
+module "nginx-pet" {
+  source = "./nginx"
 
-#   container_name = "hello-${random_pet.dog.id}"
-#   nginx_port     = 8001
-# }
+  container_name = "hello-${random_pet.dog.id}"
+  nginx_port     = 8001
+  secret_key = var.secret_key
+}
 
-# module "hello" {
-#   source  = "joatmon08/hello/random"
-#   version = "3.0.1"
 
-#   hello      = random_pet.dog.id
-#   secret_key = var.secret_key
-# }
+variable "secret_key" {
+  type = string
+  default = "secret-value"
+}
+
+module "hello" {
+  source  = "joatmon08/hello/random"
+  version = "3.0.1"
+
+  hello      = random_pet.dog.id
+  secret_key = var.secret_key
+}
 
 resource "local_file" "pet" {
   filename = "test-file-name.txt"
