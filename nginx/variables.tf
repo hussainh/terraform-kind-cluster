2a3055a0638fc6153dbfd6abaaaf777d54b82893
Hussain Hassani <hussainhassani@hotmail.com>
2023-01-07 11:46:58 +0000

    added default values to variables file and deleting unnecessary files

--- nginx/variables.tf
+++ nginx/variables.tf
@@ -1,13 +1,16 @@
 variable "container_name" {
   type = string
+  default = "docker-container1"
   description = "NGINX container's name"
 }
 
 variable "nginx_port" {
   type = number
+  default = 8000
   description = "NGINX container's external port number"
 }
 
 variable "secret_key" {
   type = string
+  default = "secret-value"
 }
