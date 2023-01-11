b11871692d01e58ae3145f9e6d20170515640981
Hussain Hassani <hussainhassani@hotmail.com>
2023-01-11 07:32:33 +0000

    fixed variable types

--- variables.tf
+++ variables.tf
@@ -1,19 +1,19 @@
 variable "kind_cluster_name" {
-  type = "string"
+  type = string
   default = "local"
 }
 
 variable "kind_cluster_config_path" {
-  type = "string"
+  type = string
   default = "~/.kube/config"
 }
 
 variable "ingress_nginx_helm_version" {
-  type = "string"
+  type = string
   default = "4.4.2"
 }
 
 variable "ingress_nginx_namespace" {
-  type = "string"
+  type = string
   default = "ingress-nginx"
 }
