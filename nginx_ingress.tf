545b6e0cc92a0e21e0fccab65d3f6735106c7c7c
Hussain Hassani <hussainhassani@hotmail.com>
2023-01-13 08:52:12 +0000

    added a null_resource block and starting to added nginx ingress values yml file (wip)

--- nginx_ingress.tf
+++ nginx_ingress.tf
@@ -10,7 +10,22 @@ resource "helm_release" "nginx_ingress" {
   chart      = "nginx-ingress"
   version    = var.ingress_nginx_helm_version
 
+  namespace = var.ingress_nginx_namespace
+  create_namespace = true
+
+  values = [file(nginx_ingress_values.yml)]
+
   depends_on = [
     kind_cluster.default
+  ]
+}
+
+resource "null_resource" "cluster" {
+  triggers = {
+    key = uuid()
+  }
+
+  depends_on = [
+    helm_release.nginx_ingress
   ]
 }
