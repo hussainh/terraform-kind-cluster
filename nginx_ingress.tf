ce85d275fa611fb2a635e89de2d4d23b35796c39
Hussain Hassani <hussainhassani@hotmail.com>
2023-01-14 08:53:25 +0000

    completed null resource

--- nginx_ingress.tf
+++ nginx_ingress.tf
@@ -13,16 +13,26 @@ resource "helm_release" "nginx_ingress" {
   namespace = var.ingress_nginx_namespace
   create_namespace = true
 
-  values = [file(nginx_ingress_values.yml)]
+  values = [file("nginx_ingress_values.yml")]
 
   depends_on = [
     kind_cluster.default
   ]
 }
 
-resource "null_resource" "cluster" {
+resource "null_resource" "wait_for_ingress_nginx" {
   triggers = {
     key = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+        printf "\nWaiting for the nginx ingress controller...\n"
+        kubectl wait --namespace ${helm_release.ingress_nginx.namespace} \
+            --for=condition=ready pod \
+            --selector=app.kubernetes.io/component=controller \
+            --timeout=90s
+        EOF
   }
 
   depends_on = [
