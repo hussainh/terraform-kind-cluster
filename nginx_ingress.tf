18d18fd9e43107269a2c9eb07101f3083c3d2ef4
Hussain Hassani <hussainhassani@hotmail.com>
2023-01-15 11:39:47 +0000

    fixed a bug with ingress-nginx set up and indentation bug in the yaml file

--- nginx_ingress.tf
+++ nginx_ingress.tf
@@ -5,9 +5,9 @@ provider "helm" {
 }
 
 resource "helm_release" "nginx_ingress" {
-  name       = "nginx-ingress"
+  name       = "ingress-nginx"
   repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "nginx-ingress"
+  chart      = "ingress-nginx"
   version    = var.ingress_nginx_helm_version
 
   namespace = var.ingress_nginx_namespace
